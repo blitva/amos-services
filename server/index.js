cfff70fec45befd1d81f3da54b4159d615833812
amosNash <66794449+amosNash@users.noreply.github.com>
2021-02-08 12:30:10 -0700

    Adding PR edits

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ app.use(cors(corsOptions));
 
 
 app.use(express.static(__dirname + '/../public'));
+app.use('/:productId', express.static(__dirname + '/../public'));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
